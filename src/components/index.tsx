e89fd8cfa2bebe735ba9a1646e460a363c62aa0f
YouYou94 <ybh4115@gmail.com>
2023-09-18 21:08:58 +0900

    init: Menu Component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,7 @@
 /* Common */
 export { Loading } from './common/loading';
 export { Header } from './common/header';
+export { Menu } from './common/menu';
 
 /* Components */
 export { Login } from './login';
