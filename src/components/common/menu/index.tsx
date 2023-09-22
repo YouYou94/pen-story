0eadd7c170f73b6677d69f34a54c8b474285333a
YouYou94 <ybh4115@gmail.com>
2023-09-22 19:57:46 +0900

    Feat: 로그인 및 비로그인 메뉴 클릭 시 작동 기능

--- src/components/common/menu/index.tsx
+++ src/components/common/menu/index.tsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from 'react';
 import { Layout, Box } from './Styled';
 
 export const Menu = () => {
+  const [isLogined, setIsLogined] = useState<boolean>(false);
+
+  const handleOnClickMenuButton = () => {};
+
+  useEffect(() => {
+    /* 로그인 여부 확인 */
+    /* 로그인이 되어 있다면 */
+    setIsLogined(true);
+  }, []);
+
   return (
     <Layout>
-      <Box>{/* 메뉴 */}</Box>
+      <Box>
+        {/* 메뉴 */}
+        {/* 로그인 여부 확인 */}
+        {isLogined ? <>{/* 로그인 메뉴 */}</> : <>{/* 비로그인 메뉴 */}</>}
+      </Box>
     </Layout>
   );
 };
+
+/*
+
+로그인 / 비로그인 메뉴 컴포넌트로 나눌지 고민해보기
+
+*/
