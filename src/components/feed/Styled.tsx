cf272a5691ae57770def5bc4c8fcd53b81a53c32
YouYou94 <ybh4115@gmail.com>
2023-10-05 22:20:53 +0900

    Design: Feed Component

--- src/components/feed/Styled.tsx
+++ src/components/feed/Styled.tsx
@@ -1,3 +1,8 @@
 import styled from '@emotion/styled';
 
-export const Layout = styled.section``;
+export const Layout = styled.section`
+  flex: 1;
+
+  display: flex;
+  flex-direction: column;
+`;
